8d91a57d65dcf2161ce7b7508782a976a10cbdef
yiral <1209997913@qq.com>
2016-01-04 11:23:37 +0800

    first

---
+++ 走起WeakDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  走起WeakDay
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 刘海艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
